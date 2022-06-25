097c2a584b01bcd1355ae547bd7e626c1f3d42ef
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-06-25 16:16:22 -0400

    Added more detail to about me page

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-const hamburger = document.querySelector('.hamburger')
-const nav = document.querySelector('.nav')
+window.onload = () => {
+    const transition_el = document.querySelector(".transition")
+    const anchors = document.querySelectorAll('a') 
+    setTimeout(() => {
+        transition_el.classList.remove('is-active')
 
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('show')
-});
+    }, 500)
+
+    // for(let i = 0; i < anchors.length; i++) {
+    //     const anchor = anchors[i];
+
+    //     anchor.addEventListener('click', e => {
+    //         e.preventDefault();
+    //         let target = e.target.href;
+
+    //         transition_el.classList.add('is-active');
+            
+    //         setTimeout(() => {
+    //             window.location.href = target;
+    //         }, 500);
+    //     });
+
+    // }
+}
+
+
+// const hamburger = document.querySelector('.hamburger')
+// const nav = document.querySelector('.nav')
+
+// hamburger.addEventListener('click', () => {
+//     nav.classList.toggle('show')
+// }); 
