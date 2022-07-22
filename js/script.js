22935036fdc4c555719608f64d673bd5b97591df
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-07-22 08:33:24 -0400

    added appear on scroll effect for section contents

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav .container ul li');
 
+// Content appear on scroll 
+window.addEventListener('scroll', reveal);
+
+
+function reveal(){
+  let reveals = document.querySelectorAll('.reveal');
+  for(let i = 0; i < reveals.length; i++){
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
 // Keeping track of scroll using an event listener 
 window.addEventListener('scroll', () => {
     // Grab the current section
@@ -20,6 +38,8 @@ window.addEventListener('scroll', () => {
         if(pageYOffset >= (sectionTop - sectionHeight/2)) {
             current = section.getAttribute('id');
         }
+
+
     })
     navLi.forEach(li => {
         li.classList.remove('active');
