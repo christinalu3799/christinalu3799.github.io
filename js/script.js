7191b501771e0111d7666e65af50e8f2fd9570e9
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-05 22:17:27 -0400

    fixed mobile project card spacing between cards

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function reveal(){
   for(let i = 0; i < reveals.length; i++){
     let windowHeight = window.innerHeight;
     let revealTop = reveals[i].getBoundingClientRect().top;
-    let revealPoint = 100;
+    let revealPoint = 75;
 
     if(revealTop < windowHeight - revealPoint){
       reveals[i].classList.add('active');
